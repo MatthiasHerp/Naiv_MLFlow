6284aa138026628168f2f92aaddfa52a5308c8bb
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2020-08-25 12:41:11 +0200

    Update Naive_MLFlow.py

--- Naive_MLFlow.py
+++ Naive_MLFlow.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[38]:
-
-
 import numpy as np
 import pandas as pd #data wrangeling
 import matplotlib
@@ -11,25 +5,9 @@ import matplotlib.pyplot as plt
 import os #for setting the working drectory
 import mlflow
 
-
-# In[39]:
-
-
-#setting working directory
-#os.chdir("C:/Users/mah/Desktop/M5_Wallmart_Challenge")
-#os.getcwd()
-
-
-# In[2]:
-
-
 #Importing evaluation metrics
 #Note: package is called scikit-learn (look for full name if i search it in anaconda)
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
-
-# In[3]:
-
 
 #defining the evaluation metrics
 #from: https://mlflow.org/docs/latest/tutorials-and-examples/tutorial.html
@@ -38,10 +16,6 @@ def eval_metrics(actual, pred):
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
-
-
-# In[36]:
-
 
 def naive_forecast(train, test, alpha):
     data = np.zeros(len(test))
@@ -56,59 +30,43 @@ def naive_forecast(train, test, alpha):
     for i in range (0,len(test)):
         naive_forecast[i] = alpha * train.revenue[len(train)-(365-1-i)]
     return naive_forecast
-    
+
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+
+        revenue_CA_1_FOODS_day = pd.read_csv(os.path.join(os.getcwd(), "revenue_CA_1_FOODS_day.csv"), 
+                                             index_col='date')
+
+        # Split the data into training and test sets
+        train = revenue_CA_1_FOODS_day.iloc[:(len(revenue_CA_1_FOODS_day)-31)]
+        test = revenue_CA_1_FOODS_day.iloc[(len(revenue_CA_1_FOODS_day)-31):]
+
+        with mlflow.start_run():
+
+            prediction = naive_forecast(train, test, alpha)
+
+            (rmse, mae, r2) = eval_metrics(test, prediction)
+
+            print("Naive Model (alpha=%f):" % (alpha))
+            print("  RMSE: %s" % rmse)
+            print("  MAE: %s" % mae)
+            print("  R2: %s" % r2)
+
+            mlflow.log_param("alpha", alpha)
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("r2", r2)
+            mlflow.log_metric("mae", mae)
+
+            #Plot the results
+
+            plt.figure(figsize=(15, 5))
+            plt.plot(test)
+            plt.plot(prediction, color="red")
+            plt.xlabel("date")
+            plt.ylabel("revenue_CA_1_FOODS")
+            plt.legend(("realization", "prediction"),  
+                   loc="upper left")
+            plt.savefig('prediction_plot.png')
 
 
-# In[43]:
-
-
-def Naive_Model(alpha):
-
-    # Read in data
-    
-    revenue_CA_1_FOODS_day = pd.read_csv(os.path.join(os.getcwd(), "revenue_CA_1_FOODS_day.csv"), 
-                                         index_col='date')
-
-
-    # Split the data into training and test sets
-    train = revenue_CA_1_FOODS_day.iloc[:(len(revenue_CA_1_FOODS_day)-31)]
-    test = revenue_CA_1_FOODS_day.iloc[(len(revenue_CA_1_FOODS_day)-31):]
-
-    with mlflow.start_run():
-        
-        prediction = naive_forecast(train, test, alpha)
-
-        (rmse, mae, r2) = eval_metrics(test, prediction)
-
-        print("Naive Model (alpha=%f):" % (alpha))
-        print("  RMSE: %s" % rmse)
-        print("  MAE: %s" % mae)
-        print("  R2: %s" % r2)
-
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_metric("rmse", rmse)
-        mlflow.log_metric("r2", r2)
-        mlflow.log_metric("mae", mae)
-        
-        #Plot the results
-        
-        plt.figure(figsize=(15, 5))
-        plt.plot(test)
-        plt.plot(prediction, color="red")
-        plt.xlabel("date")
-        plt.ylabel("revenue_CA_1_FOODS")
-        plt.legend(("realization", "prediction"),  
-               loc="upper left")
-        plt.savefig('prediction_plot.png')
-        
-        
-        mlflow.log_artifact("./prediction_plot.png")
-
-
-# In[45]
-
-# In[ ]:
-
-
-
-
+            mlflow.log_artifact("./prediction_plot.png")
