1507210fae69f422261d1c912380096756a31e3c
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2020-08-25 17:47:08 +0200

    Update Naive_MLFlow.py

--- Naive_MLFlow.py
+++ Naive_MLFlow.py
@@ -19,7 +19,7 @@ def eval_metrics(actual, pred):
     r2 = r2_score(actual, pred)
     return rmse, mae, r2
 
-def naive_forecast(train, test):
+def naive_forecast(train, test, alpha):
     data = np.zeros(len(test))
     #create the naive forecasting series with the index identical to the days to forecast which are the test data set index
     naive_forecast = pd.Series(data,index=test.index)
@@ -30,7 +30,7 @@ def naive_forecast(train, test):
     #then an additional minus 1 as 2016 is a leap year
     #i runs from 0 to 30 so that the horizon we go back deminishes as we forecast further and further
     for i in range (0,len(test)):
-        naive_forecast[i] = train.revenue[len(train)-(365-1-i)]
+        naive_forecast[i] = alpha * train.revenue[len(train)-(365-1-i)]
     return naive_forecast
 
 
@@ -54,31 +54,27 @@ if __name__ == "__main__":
     #alpha = float(sys.argv[1]) #if len(sys.argv) > 1 else 0.5
 
     with mlflow.start_run():
+        prediction = naive_forecast(train, test, alpha)
+        (rmse, mae, r2) = eval_metrics(test, prediction)
 
-                prediction = naive_forecast(train, test)
+        print("Naive Model (alpha=%f):" % (alpha))
+        print("  RMSE: %s" % rmse)
+        print("  MAE: %s" % mae)
+        print("  R2: %s" % r2)
 
-                (rmse, mae, r2) = eval_metrics(test, prediction)
+        #mlflow.log_param("alpha", alpha)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
 
-                #print("Naive Model (alpha=%f):" % (alpha))
-               # print("  RMSE: %s" % rmse)
-                #print("  MAE: %s" % mae)
-               # print("  R2: %s" % r2)
+        #Plot the results
 
-                #mlflow.log_param("alpha", alpha)
-                mlflow.log_metric("rmse", rmse)
-                mlflow.log_metric("r2", r2)
-                mlflow.log_metric("mae", mae)
-
-                #Plot the results
-
-                plt.figure(figsize=(15, 5))
-                plt.plot(test)
-                plt.plot(prediction, color="red")
-                plt.xlabel("date")
-                plt.ylabel("revenue_CA_1_FOODS")
-                plt.legend(("realization", "prediction"),  
-                       loc="upper left")
-                plt.savefig('prediction_plot.png')
-
-
-                mlflow.log_artifact("./prediction_plot.png")
+        plt.figure(figsize=(15, 5))
+        plt.plot(test)
+        plt.plot(prediction, color="red")
+        plt.xlabel("date")
+        plt.ylabel("revenue_CA_1_FOODS")
+        plt.legend(("realization", "prediction"), loc="upper left")
+        plt.savefig('prediction_plot.png')
+        
+        mlflow.log_artifact("./prediction_plot.png")
