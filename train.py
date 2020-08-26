d20a6d7426fc20832b373a77bdbfa32f046a1fdb
MatthiasHerp <63145900+MatthiasHerp@users.noreply.github.com>
2020-08-26 11:05:50 +0200

    Update train.py

--- train.py
+++ train.py
@@ -15,25 +15,25 @@ import matplotlib
 
 #defining the evaluation metrics
 #from: https://mlflow.org/docs/latest/tutorials-and-examples/tutorial.html
-#def eval_metrics(actual, pred):
-#    rmse = np.sqrt(mean_squared_error(actual, pred))
-#    mae = mean_absolute_error(actual, pred)
-#    r2 = r2_score(actual, pred)
-#    return rmse, mae, r2
+def eval_metrics(actual, pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse, mae, r2
 
-#def naive_forecast(train, test, alpha):
-#    data = np.zeros(len(test))
-#    #create the naive forecasting series with the index identical to the days to forecast which are the test data set index
-#    naive_forecast = pd.Series(data,index=test.index)
-#    #loop goes threw the complete data and gets forecasts
-#    #we take the length thus the last day
-#    #minus 365 to get a year back
-#    #then minus one to get the same week day of the past year
-#    #then an additional minus 1 as 2016 is a leap year
-#    #i runs from 0 to 30 so that the horizon we go back deminishes as we forecast further and further
-#    for i in range (0,len(test)):
-#        naive_forecast[i] = alpha * train.revenue[len(train)-(365-1-i)]
-#    return naive_forecast
+def naive_forecast(train, test, alpha):
+    data = np.zeros(len(test))
+    #create the naive forecasting series with the index identical to the days to forecast which are the test data set index
+    naive_forecast = pd.Series(data,index=test.index)
+    #loop goes threw the complete data and gets forecasts
+    #we take the length thus the last day
+    #minus 365 to get a year back
+    #then minus one to get the same week day of the past year
+    #then an additional minus 1 as 2016 is a leap year (here 2015so no leap year)
+    #i runs from 0 to 30 so that the horizon we go back deminishes as we forecast further and further
+    for i in range (0,len(test)):
+        naive_forecast[i] = alpha * train.revenue[len(train)-(365-1-i)]
+    return naive_forecast
 
 
 
@@ -41,22 +41,22 @@ if __name__ == "__main__":
     #warnings.filterwarnings("ignore")
     #np.random.seed(40)
     
-    #revenue_CA_1_FOODS_day = os.path.join(os.path.dirname(os.path.abspath(__file__)), "revenue_CA_1_FOODS_day.csv")
+    revenue_CA_1_FOODS_day = os.path.join(os.path.dirname(os.path.abspath(__file__)), "revenue_CA_1_FOODS_day.csv")
 
-    #revenue_CA_1_FOODS_day = pd.read_csv(revenue_CA_1_FOODS_day, index_col='date')
+    revenue_CA_1_FOODS_day = pd.read_csv(revenue_CA_1_FOODS_day, index_col='date')
 
-    #revenue_CA_1_FOODS_day = pd.read_csv(os.path.join(os.getcwd(), "revenue_CA_1_FOODS_day.csv"), 
-    #                                     index_col='date')
+    revenue_CA_1_FOODS_day = pd.read_csv(os.path.join(os.getcwd(), "revenue_CA_1_FOODS_day.csv"), 
+                                         index_col='date')
 
     # Split the data into training and test sets
-    #train = revenue_CA_1_FOODS_day.iloc[:(len(revenue_CA_1_FOODS_day)-31)]
-    #test = revenue_CA_1_FOODS_day.iloc[(len(revenue_CA_1_FOODS_day)-31):]
+    train = revenue_CA_1_FOODS_day.iloc[:(len(revenue_CA_1_FOODS_day)-31)]
+    test = revenue_CA_1_FOODS_day.iloc[(len(revenue_CA_1_FOODS_day)-31):]
     
     #alpha = float(sys.argv[1]) #if len(sys.argv) > 1 else 1
 
     with mlflow.start_run():
-        #prediction = naive_forecast(train, test, alpha)
-        #(rmse, mae, r2) = eval_metrics(test, prediction)
+        prediction = naive_forecast(train, test, alpha)
+        (rmse, mae, r2) = eval_metrics(test, prediction)
 
         #print("Naive Model (alpha=%f):" % (alpha))
         #print("  RMSE: %s" % rmse)
@@ -64,9 +64,9 @@ if __name__ == "__main__":
         #print("  R2: %s" % r2)
 
         mlflow.log_param("alpha", 12)
-        #mlflow.log_metric("rmse", rmse)
-       #mlflow.log_metric("r2", r2)
-        #mlflow.log_metric("mae", mae)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
 
         #Plot the results
 
